f881a97269422e79bd8ba5fd7afbe7ac3980f67e
Icelycoool <mohammedcali@proton.me>
2024-07-13 16:08:50 -0700

    Finished all the unittests for the FileStorage class

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -27,5 +27,22 @@ class TestFileStorage(unittest, TestCase):
         """Test the all method"""
         self.assertEqual(self.storage.all(), {})
 
+    def test_new(self):
+        """Test the new method"""
+        mode = BaseModel()
+        self.storage.new(model)
+        self.storage.save()
+        self.storage.save()
+
+    def test_reload(self):
+        """Test the reload method"""
+        model = BaseModel()
+        self.storage.new(model)
+        self.storage.save()
+        self.storage.reload
+        key = f"BaseModel.{model.id}"
+        self.assertIn(key, self.storage.all())
+
+
 if __name__ == '__main__':
     unittest.main()
