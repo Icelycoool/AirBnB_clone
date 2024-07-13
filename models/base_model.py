a51bf37fe429b02b984424366a14531426f51e0c
Icelycoool <mohammedcali@proton.me>
2024-07-13 15:12:59 -0700

    Created a stroga module that stores the objects

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ Defined the base model for the AirBNB_clone
 """
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -30,6 +31,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """
@@ -46,6 +48,7 @@ class BaseModel:
         updated_at with the current datetime.
         """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
---
+++ models/engine/file_storage.py
@@ -0,0 +1,55 @@
+#!/bin/usr/bin/python3
+"""
+Module FileStorage that serializes instances to a
+JSON file and deserializes JSON file to instances.
+"""
+
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """
+    Custom class for File Storage.
+    """
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """
+        Returns dictionary representation of all onjects
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """
+        Sets in __objects the obj with key <obj class name>.id
+        """
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """
+        Serializes __objects to the JSON file (path: __file_path).
+        """
+        obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
+        with open(self.__file_path, 'w') as file:
+            json.dump(obj_dict, file)
+
+    def reload(self):
+        """
+        Deserializes the JSON file to __objects.
+        """
+        try:
+            with open(self.__file_path, 'r') as file:
+                obj_dict = json.load(file)
+                for key, val in obj_dict.items():
+                    class_name, obj_id = key.split('.')
+
+                    if class_name == "BaseModel":
+                        obj_instance = BaseModel(**val)
+
+                    self.__objects[key] = obj_instance
+        except FileNotFoundError:
+            pass
