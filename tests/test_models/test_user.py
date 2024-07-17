1f57bb4b558a17c8e80e976d53e84f88ecb20bc5
Icelycoool <mohammedcali@proton.me>
2024-07-17 16:44:16 -0700

    Added the unittests for the user class

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -2,39 +2,62 @@
 """
 Unittest for the user class.
 """
+import os
 import unittest
+from models.engine.file_storage import FileStorage
 from models.user import User
+from models import storage
 
 
 class TestUser(unittest.TestCase):
     """Tests the User class"""
 
     def setUp(self):
-        pass
+        self.user = User()
 
-    def test_email_assignment(self):
-        """Test User email assignment"""
-        user = User()
-        user.email = "user@email.com"
-        self.assertEqual(user.email, "user@email.com")
+    def tearDown(self):
+        del self.user
+        FileStorage._FileStorage__objects = {}
+        if os.path.exists(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
 
-    def test_password_assignment(self):
-        """Test User password assignment"""
-        user = User()
-        user.password = "password"
-        self.assertEqual(user.password, "password")
+    def test_attributes(self):
+        """Test User attributes"""
+        self.assertTrue(hasattr(self.user, "email"))
+        self.assertTrue(hasattr(self.user, "password"))
+        self.assertTrue(hasattr(self.user, "first_name"))
+        self.assertTrue(hasattr(self.user, "last_name"))
 
-    def test_first_name_assignment(self):
-        """Test User first_name assignment"""
-        user = User()
-        user.first_name = "John"
-        self.assertEqual(user.first_name, "John")
+    def test_params(self):
+        """Test User parameters"""
+        self.assertIsInstance(self.user.email, str)
+        self.assertIsInstance(self.user.password, str)
+        self.assertIsInstance(self.user.first_name, str)
+        self.assertIsInstance(self.user.last_name, str)
 
-    def test_last_name_assignment(self):
-        """Test User last_name assignment"""
-        user = User()
-        user.last_name = "Doe"
-        self.assertEqual(user.last_name, "Doe")
+    def test_save_method(self):
+        """Test sav emethod"""
+        self.user.save()
+        self.assertTrue(os.path.exists(FileStorage._FileStorage__file_path))
+
+    def test_to_dict_method(self):
+        """Test to_dict method"""
+        self.user.email = "test@example.com"
+        self.user.password = "password"
+        self.user.first_name = "John"
+        self.user.last_name = "Doe"
+        user_dict = self.user.to_dict()
+        self.assertEqual(user_dict["email"], "test@example.com")
+        self.assertEqual(user_dict["password"], "password")
+        self.assertEqual(user_dict["first_name"], "John")
+        self.assertEqual(user_dict["last_name"], "Doe")
+        self.assertEqual(user_dict["__class__"], "User")
+
+    def test_storage_all(self):
+        """Test that storage.all() contains the new user"""
+        self.user.save()
+        self.assertIn(self.user, storage.all().values())
+
 
 if __name__ == "__main__":
     unittest.main()
